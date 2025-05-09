42ed1db9c23b80d22c6ceeda4c072dce4ebaf62f
Kirtoisplayz <omgitsdarshan@gmail.com>
2025-05-09 15:21:41 -0700

    Update AuthForm

--- app/components/AuthForm.jsx
+++ app/components/AuthForm.jsx
@@ -1,10 +1,8 @@
 'use client'
 
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { supabase } from "../utils/supabaseClient"
 import { useRouter } from "next/navigation"
-
-
 
 export default function AuthForm(){
     const [isNewUser, setIsNewUser] = useState(false)
@@ -18,23 +16,39 @@ export default function AuthForm(){
     const [otp, setOtp] = useState('')
     const router = useRouter()
 
+    useEffect(() => {
+        // Check for the magic link token in the URL
+        const handleMagicLink = async () => {
+            const { error } = await supabase.auth.getSessionFromUrl({ storeSession: true });
 
-async function handleLogin(e) {
-  e.preventDefault()
-  setIsSigningIn(true)
+            if (!error) {
+                // Redirect to the main page after successful login
+                router.push("/");
+            } else {
+                console.error("Error handling magic link:", error.message);
+            }
+        };
 
-  const { error } = await supabase.auth.signInWithOtp({
-    email
-  })
+        handleMagicLink();
+    }, [router]);
 
-  if (!error) {
-    setEmailSent(true)
-  } else {
-    console.log('Login error:', error.message)
-  }
+    async function handleLogin(e) {
+        e.preventDefault();
+        setIsSigningIn(true);
 
-  setIsSigningIn(false)
-}
+        // Use Supabase's signInWithOtp to send a login email
+        const { error } = await supabase.auth.signInWithOtp({
+            email,
+        });
+
+        if (!error) {
+            setEmailSent(true); // Notify the user that the email has been sent
+        } else {
+            console.error('Login error:', error.message);
+        }
+
+        setIsSigningIn(false);
+    }
 
     async function handleSignUp(e){
         e.preventDefault();
@@ -58,63 +72,62 @@ async function handleLogin(e) {
 
     const signUpMessage = <p className="text-center text-white">Email sent! Check your email to confirm sign up.</p>
 
-return (
-    <>
-    {emailSent ? (
-      <p className="text-center text-white">
-        A login link has been sent to your email. Please check your inbox to complete the login.
-      </p>
-    ) : (
-        <form onSubmit={isNewUser ? handleSignUp : handleLogin} className="space-y-8">
-            <input
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="appearance-none rounded relative block w-full px-3 py-2 border placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-                placeholder="Email"
-            />
-            <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="appearance-none rounded relative block w-full px-3 py-2 border placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-                placeholder="Password"
-            />
-            <button
-                type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
-            >
-                {signInMessage}
-            </button>
-            <p className="text-center text-white">
-                {isNewUser ? (
-                <>
-                    Already have an account? {' '}
-                    <button
-                    type="button"
-                    onClick={() => setIsNewUser(false)}
-                    className="text-indigo-400 hover:text-indigo-600"
-                    >
-                    Sign In
-                    </button>
-                </>
-                ) : (
-                <>
-                    Don't have an account? {' '}
-                    <button
-                    type="button"
-                    onClick={() => setIsNewUser(true)}
-                    className="text-indigo-400 hover:text-indigo-600"
-                    >
-                    Sign Up
-                    </button>
-                </>
-                )}
-            </p>
-            {isSigningUp && signUpMessage}
-        </form>
-    )}
-    </>
-)
-
+    return (
+        <>
+        {emailSent ? (
+        <p className="text-center text-white">
+            A login link has been sent to your email. Please check your inbox to complete the login.
+        </p>
+        ) : (
+            <form onSubmit={isNewUser ? handleSignUp : handleLogin} className="space-y-8">
+                <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="appearance-none rounded relative block w-full px-3 py-2 border placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                    placeholder="Email"
+                />
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="appearance-none rounded relative block w-full px-3 py-2 border placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                    placeholder="Password"
+                />
+                <button
+                    type="submit"
+                    className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+                >
+                    {signInMessage}
+                </button>
+                <p className="text-center text-white">
+                    {isNewUser ? (
+                    <>
+                        Already have an account? {' '}
+                        <button
+                        type="button"
+                        onClick={() => setIsNewUser(false)}
+                        className="text-indigo-400 hover:text-indigo-600"
+                        >
+                        Sign In
+                        </button>
+                    </>
+                    ) : (
+                    <>
+                        Don't have an account? {' '}
+                        <button
+                        type="button"
+                        onClick={() => setIsNewUser(true)}
+                        className="text-indigo-400 hover:text-indigo-600"
+                        >
+                        Sign Up
+                        </button>
+                    </>
+                    )}
+                </p>
+                {isSigningUp && signUpMessage}
+            </form>
+        )}
+        </>
+    )
 }
